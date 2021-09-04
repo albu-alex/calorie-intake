80ba8eb2949cb47dbb61d150b506eb84ff289e3c
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-09-04 14:09:55 +0300

    basic layout complete

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <view class="header">
+    <text class="title">{{title}}</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ['title']
+}
+</script>
+
+<style>
+.header{
+  height: 60px;
+  background-color: black;
+  align-items: center;
+  justify-content: center;
+}
+.title{
+  font-size: 28px;
+  font-weight: 500;
+  color: ghostwhite;
+}
+</style>
---
+++ components/StatusBar.vue
@@ -0,0 +1,18 @@
+<!--Component is only shown in mobile version-->
+<!--Replaces the standard iOS status bar with a black one-->
+<template>
+  <view class="statusbar"></view>
+</template>
+
+<script>
+export default {
+  name: "StatusBar"
+}
+</script>
+
+<style>
+.statusbar{
+  height: 20px;
+  background-color: #000000;
+}
+</style>
